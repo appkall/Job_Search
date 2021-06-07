0cb93705ff73f4bfcc3943c0b2b6f20fc66d11a9
Luis Berrizbeitia <26914573+appkall@users.noreply.github.com>
2021-06-07 10:52:44 -0500

    Updates

--- main.py
+++ main.py
@@ -1,6 +1,24 @@
 #2021 Attempt
 
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+import ssl
 import docx2txt
 
-document = docx2txt.process('test.docx')
-print(document)
+
+resume = docx2txt.process('test.docx')
+#print(resume)
+
+
+# Ignore SSL certificate errors
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
+
+#url = input('Enter - ')
+url = 'https://uscareers-lewisenergy.icims.com/jobs/9011/senior-drilling-engineer/job'
+html = urlopen(url, context=ctx).read()
+soup = BeautifulSoup(html, "html.parser")
+
+
+print(soup)
